de4e1333b80940455b5f5e6ff1e7a924b5a53dea
Fishka96 <123028963+Fishka96@users.noreply.github.com>
2023-01-18 22:58:14 +0100

    Fcommit
    Calculation commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 235;
+        int b = 152;
+
+        int c = a + b;
+        System.out.println("Сложение а и б составляет = " + c);
+
+    }
+
+}
